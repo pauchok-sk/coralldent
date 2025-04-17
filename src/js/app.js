edd112b737326635679169b0199d6fbce5b4846e
pauchok-sk <pauchoooooook@gmail.com>
2025-04-17 16:46:01 +0300

    reviews

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import "../scss/style.scss";
 import burger from "./files/burger.js";
 import linkHover from "./files/linkHover.js";
+import mediaAdaptive from "./files/mediaAdaptive.js";
 import sliders from "./files/sliders.js";
 import spoller from "./files/spoller.js";
 
@@ -8,5 +9,10 @@ spoller();
 linkHover();
 sliders();
 burger();
+mediaAdaptive();
 
-Fancybox.bind("[data-fancybox]");
+Fancybox.bind("[data-fancybox]", {
+  closeButton: false
+});
+
+// Fancybox.show([{src: "#modal-doctor", type: "inline"}])
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -64,4 +64,39 @@ export default function sliders() {
       },
     });
   }
+
+  const reviewsSlider = document.querySelector(".s-reviews__slider");
+
+  if (reviewsSlider) {
+    const slider = new Swiper(reviewsSlider, {
+      speed: 700,
+      slidesPerView: 1,
+      spaceBetween: 16,
+      autoplay: {
+        delay: 3000,
+      },
+      navigation: {
+        nextEl: ".s-reviews .slider-btn._next",
+        prevEl: ".s-reviews .slider-btn._prev",
+      },
+      pagination: {
+        el: ".s-reviews .slider-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 16,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 16,
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 16,
+        }
+      },
+    });
+  }
 }
